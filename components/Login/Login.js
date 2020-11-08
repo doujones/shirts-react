dbcfa4d96c166eafd1d0e56c41edf4ca39ecf0f1
Dou Jones <MISTERCHIEFEXECUTIVEOFFICER@YAHOO.COM>
2020-11-08 13:17:25 -0500

    updated

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -3,7 +3,29 @@ import './Login.css'
 
 
 const Login = () => {
-  
+  return (
+      <div className="login">
+          <form className="login-form">
+              <h1>Boswell's E-Commerce</h1>
+          <input className="input-text" 
+          type="email"
+          placeholder="email address"
+          name="email"
+          required
+          />
+          <input className="input-text" 
+          type="password"
+          placeholder="password"
+          name="password"
+          required
+          />
+          <input className="input-submit" 
+          type="submit"
+          value="login"
+          />
+          </form>
+      </div>
+  )
 }
 
 export default Login;
